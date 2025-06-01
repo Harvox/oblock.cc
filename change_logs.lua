8d07b8008c753505d2566497917cfcaa92953315
Harvox <134923684+Harvox@users.noreply.github.com>
2025-06-01 04:33:25 -0600

    Update change_logs.lua

--- change_logs.lua
+++ change_logs.lua
@@ -1,7 +1,8 @@
 return {
   ["Tha Bronx"] = {
-    "- Fixed loader not popping up.",
+    "- Fixed loader not popping up",
     "- Fixed tp bypass on Solara",
     "- Fixed autofarms on Solara",
+    "- New tp bypass",
   };
 }
