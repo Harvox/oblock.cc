37ff571c52ab36f9e7e3287fd6851df23dff4fcd
Harvox <134923684+Harvox@users.noreply.github.com>
2025-07-19 11:53:21 -0600

    Update change_logs.lua

--- change_logs.lua
+++ change_logs.lua
@@ -1,22 +1,8 @@
 return {
   ["Tha Bronx"] = {
-    "- Fixed loader not popping up",
-    "- Fixed tp bypass on Solara",
-    "- Fixed autofarms on Solara",
-    "- New tp bypass",
-    "- Safezone checks are fixed",
-    "- Gun Mods are fixed",
-    "- Infinite money & dupe fixed",
-    "- New Visuals",
-    "- New ESP",
-    "- Fixed features not working on mobile",
-    "- UI reworked",
+    "- No new updates...",
   };
   ["South Bronx"] = {
-    "- New ESP",
-    "- Crashing resolved",
-    "- Removed Rollback Dupe",
-    "- Added Instant Kill to Players",
-    "- UI reworked",
+    "- No new updates...",
   }
 }
